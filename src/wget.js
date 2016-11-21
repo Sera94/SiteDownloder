2d53aef105550a08ea88be7ebb065997a9634f65
shenjiafei <2572790646@qq.com>
2016-11-21 10:01:08 +0800

    uodate

---
+++ src/wget.js
@@ -0,0 +1,14 @@
+
+module.exports = function (url) {
+    return new Promise(function (ok, rej) {
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            ok(xhr.responseText);
+        };
+        xhr.onerror = function (e) {
+            rej(e);
+        };
+        xhr.open("GET", url);
+        xhr.send();
+    });
+};
